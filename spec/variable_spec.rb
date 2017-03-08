8a1379ba8d01e79721aa95a4146fdd78deada6dc
Alexandria  Baranova <alexandrialmaloney@gmail.com>
2017-03-08 14:26:48 +0000

    Done.

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,10 +1,1 @@
-# Code your solution in variable.rb
-describe "./variable.rb" do
-
-  greeting = "Hello World" do
-    greeting = "Hello World"
-
-    greeting= "Hello World"
-  end
-
-end
+greeting= "Hello World"
